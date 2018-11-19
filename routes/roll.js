29645b0b984bdafb69b41f27c1fb717b8807dbdd
Courtenay <cr.mansel@gmail.com>
2018-11-19 12:27:30 +0000

    Remove at lng hardcoding

--- routes/roll.js
+++ routes/roll.js
@@ -4,8 +4,6 @@ const roll = (req, res, next) => {
   let {body: {lat, lng}} = req
   console.log(lat)
   console.log(lng)
-  lat = 51.5207258
-  lng = -0.0935002
   return generateSuggestion(lat, lng).then(suggestion => {
     console.log(suggestion)
     res.status(200).send(suggestion)
